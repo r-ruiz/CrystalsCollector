7d72eaf6b4d1259ebed65d3ba2cf033e69cfdee6
ruben-r <ruben.ruiz@outlook.com>
2018-04-26 12:45:22 -0500

    Fixed logic to get the game to pass a win however reset function still need work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
 var MIN = 19; //start range for the computer random number
 var MAX = 120; //end range for the computer random number
-var userTotal = "";
-var userNum = "";
+var userTotal = 0;
+var userNum = 0;
 var wins = 0;
 var losses = 0;
 var computerNum = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
 
 $(document).ready(function(){
-    var userTotal = "";
-    var userNum = "";
+    var userTotal = 0;
     // set random value for each gem
     $(".jem").each(function(){
         var random = Math.floor(Math.random() * 12) + 1;
@@ -18,28 +17,45 @@ $(document).ready(function(){
 });
 
 function logic(){
-    $(".jem").on("click", function() {
-        userTotal += parseInt(this.val());
+    $("img").on("click", function() {
+        //console.log(this).val();
+        var userNum = $(this).attr("value");
+        userTotal = userTotal + parseInt(userNum);
+        //userTotal += parseInt(this.value);
         console.log(userTotal);
+        console.log(userNum);
         $("#userNumber").html("<h3>" + userTotal + "</h3>");
         if (userTotal === computerNum){
-            wins += wins;
+            wins++;
             $("#status").html("<h3>You Won!</h3>");
             $("#wins").html("<h3>" + wins + "</h3>");
             reset();
         }
         else if (userTotal > computerNum){
-            losses += losses;
+            losses++;
             $("#status").html("<h3>You Lost!</h3>");
             $("losses").html("<h3>" + losses + "</h3>");
             reset();
         }
-        
     });
-    console.log(userTotal);
 }
 
-// reset (){
-    
-// }
+function reset(){
+    //flush the following ids
+    $("#status, #userNumber").empty();
+    //new random number
+    $(".jem").each(function(){
+        var random = Math.floor(Math.random() * 12) + 1;
+        $(this).attr("value", random);
+    })
+    var computerNum = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
+    $("#compRandom").html("<h3>" + computerNum + "</h3>");
+    $("#userNumber").html("<h3>---</h3>");
+    var userTotal = 0;
+    var userNum = 0;
+    logic();
+}
+
 logic();
+
+
