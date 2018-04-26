568ed71c973590d61b3564b2afe6859d128e7bc7
ruben-r <ruben.ruiz@outlook.com>
2018-04-26 18:33:50 -0500

    fixed logic, created new functions for game and logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,55 +7,60 @@ var losses = 0;
 var computerNum = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
 
 $(document).ready(function(){
-    var userTotal = 0;
     // set random value for each gem
     $(".jem").each(function(){
         var random = Math.floor(Math.random() * 12) + 1;
         $(this).attr("value", random);
     })
     $("#compRandom").html("<h3>" + computerNum + "</h3>");
+    game();
 });
 
-function logic(){
+function game(){ 
     $("img").on("click", function() {
         //console.log(this).val();
-        var userNum = $(this).attr("value");
+        userNum = $(this).attr("value");
         userTotal = userTotal + parseInt(userNum);
         //userTotal += parseInt(this.value);
-        console.log(userTotal);
-        console.log(userNum);
+        console.log("Value of Gem: " + userNum);
+        console.log("total: " + userTotal);
+        console.log("Target is: " + computerNum);
         $("#userNumber").html("<h3>" + userTotal + "</h3>");
-        if (userTotal === computerNum){
-            wins++;
-            $("#status").html("<h3>You Won!</h3>");
-            $("#wins").html("<h3>" + wins + "</h3>");
-            reset();
-        }
-        else if (userTotal > computerNum){
-            losses++;
-            $("#status").html("<h3>You Lost!</h3>");
-            $("losses").html("<h3>" + losses + "</h3>");
-            reset();
-        }
+        logic();
     });
 }
 
+function logic(){
+    if (userTotal > computerNum){
+        losses++;
+        $("#status").html("<h4>You lost that last game</h4>");
+        $("#losses").html("<h3>" + losses + "</h3>");
+        reset();
+    }
+
+    else if (userTotal === computerNum){
+        wins++;
+        $("#status").html("<h4>You won that last game</h4>");
+        $("#wins").html("<h3>" + wins + "</h3>");
+        reset();
+    }
+}
+
 function reset(){
+    userTotal = 0;
+    userNum = 0;
     //flush the following ids
-    $("#status, #userNumber").empty();
+    $("#userNumber").empty();
     //new random number
     $(".jem").each(function(){
         var random = Math.floor(Math.random() * 12) + 1;
         $(this).attr("value", random);
     })
-    var computerNum = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
+    computerNum = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
     $("#compRandom").html("<h3>" + computerNum + "</h3>");
     $("#userNumber").html("<h3>---</h3>");
-    var userTotal = 0;
-    var userNum = 0;
-    logic();
+    return;
 }
 
-logic();
 
 
